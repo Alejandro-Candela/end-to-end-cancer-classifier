0478a05d0736c0d34dd1d5ff9001e5935a3ed8f0
alex-candela <alex.candela@outlook.com>
2024-12-14 20:04:29 +0100

    added - train pipeline in main

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
-
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
@@ -19,7 +17,6 @@ except Exception as e:
     raise e
 
 
-
 STAGE_NAME = "Prepare base model"
 try: 
    logger.info(f"*******************")
@@ -28,5 +25,17 @@ try:
    prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-        logger.exception(e)
-        raise e
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
