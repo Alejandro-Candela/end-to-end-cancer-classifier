be6edb80a80c2445c99593f0db42f2a64b477cd5
alex-candela <alex.candela@outlook.com>
2024-12-24 13:40:31 +0100

    updated - format document

--- src/cnnClassifier/pipeline/stage_03_model_trainer.py
+++ src/cnnClassifier/pipeline/stage_03_model_trainer.py
@@ -3,9 +3,7 @@ from cnnClassifier.components.model_trainer import Training
 from cnnClassifier import logger
 
 
-
 STAGE_NAME = "Training"
-
 
 
 class ModelTrainingPipeline:
@@ -21,15 +19,13 @@ class ModelTrainingPipeline:
         training.train()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        logger.info(f"*******************")
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        logger.info("*******************")
+        logger.info(">>>>>> stage %s started <<<<<<", STAGE_NAME)
         obj = ModelTrainingPipeline()
         obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        logger.info(">>>>>> stage %s completed <<<<<<\n\nx==========x", STAGE_NAME)
     except Exception as e:
         logger.exception(e)
         raise e
-        
